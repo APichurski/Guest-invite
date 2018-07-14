9fcd703b032b0efe5bafba8fcbfff6633d23f472
PArek <arekpichurski@gmail.com>
2018-07-14 21:01:04 +0200

    Adding new class GuestResponse

---
+++ TimeSheetList/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeSheetList.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        private string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your surname")]
+        private string Surname { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
+        private int Phone { get; set; }
+        [Required(ErrorMessage = "Please enter your choice")]
+        private bool WillAttend { get; set; }
+
+    }
+}
