b8f0875221b99ffd8b984cff7e93baf5ec63892d
FWidel <widelfranciszek@gmail.com>
2018-07-15 01:11:00 +0200

    Added XMLHttpRequest with controller api/2

--- TimeSheetList/ClientApp/components/VisitingCard.tsx
+++ TimeSheetList/ClientApp/components/VisitingCard.tsx
@@ -2,9 +2,19 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 
 export class VisitingCard extends React.Component<RouteComponentProps<{}>, {}> {
+
+
+
+    wypelnijCos() {
+        var request = new XMLHttpRequest();
+        request.open('POST', '/api/2', true);
+        request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        request.send('{"Name": "Kamil"}');
+    }
+
     public render() {
         return <div>
-            <h1>Strona z wizytowka do wypelnienia</h1>
+            <h1 onClick={this.wypelnijCos}>Strona z wizytowka do wypelnienia</h1>
         </div>;
     }
 }
