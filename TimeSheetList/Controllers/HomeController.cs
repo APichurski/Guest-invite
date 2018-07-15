31d75e958fff9ae8427b4263128dbae3d5502861
PArek <arekpichurski@gmail.com>
2018-07-15 16:34:26 +0200

    fixed a lot of bugs!!!

--- TimeSheetList/Controllers/HomeController.cs
+++ TimeSheetList/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using TimeSheetList.Models;
 
@@ -23,22 +24,43 @@ namespace TimeSheetList.Controllers
         }
 
         [Route("api/1")]
-        public IActionResult Test()
+        public async Task<IActionResult> SearchData(string _name)
         {
            
 
             string connectionString = "mongodb+srv://admin:admin@cluster0-s6j4s.mongodb.net/test?retryWrites=true";
 
 
-            GuestResponse ob1 = new GuestResponse { Name = "777777777" };
+            
             var client = new MongoClient(connectionString);
             var database = client.GetDatabase("PartyCard");
             var collection = database.GetCollection<GuestResponse>("NumberOfguest");
-            collection.InsertOneAsync(ob1);
-            return Ok(ob1);
+           
+
+            var filter = Builders<GuestResponse>.Filter.Eq("Name", _name);
+            var list = await collection.Find(new BsonDocument()).ToListAsync();
+            return Ok(list);
 
 
          }
+        [Route("api/3")]
+        public IActionResult DeleteData(string _name = "Kamil")
+        {
+
+
+            string connectionString = "mongodb+srv://admin:admin@cluster0-s6j4s.mongodb.net/test?retryWrites=true";
+
+
+
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("PartyCard");
+            var collection = database.GetCollection<GuestResponse>("NumberOfguest");
+            var filter = Builders<GuestResponse>.Filter.Eq("Name", "Kamil");
+            var result = collection.DeleteMany(filter);
+            return Ok(result);
+
+
+        }
         [Route("api/2")]
         [HttpPost]
         public IActionResult RecevingData([FromBody]GuestResponse oneguest)
