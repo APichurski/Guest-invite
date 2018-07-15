6e584768d4406f7f74be3fe1166891be69ad3c23
FWidel <widelfranciszek@gmail.com>
2018-07-15 14:17:57 +0200

    Add tablewith list of all guest add popup window after inserting guest add clearing form after add guest

--- TimeSheetList/ClientApp/components/VisitingCard.tsx
+++ TimeSheetList/ClientApp/components/VisitingCard.tsx
@@ -13,11 +13,22 @@ export class VisitingCard extends React.Component<RouteComponentProps<{}>, {}> {
         var surname = z.target.surname.value;
         var phone = z.target.phone.value;
         var attendace = z.target.willAttend.value == "on" ? true : false;
-        request.send('{"Name":' + name + ', "Surname":' + surname + ', "Phone":' + phone + ', "WillAttend":'+ attendace + ' }');
+
+        z.target.reset();
+
+        request.onload = function () {
+            // do something to response
+            console.log(request.responseText);
+        };
+
+     
+
+        request.send('{"Name":' + name + ', "Surname":' + surname + ', "Phone":' + phone + ', "WillAttend":' + attendace + ' }');
     }
 
     render() {
         return (
+            <div>
             <form onSubmit={this.wypelnijCos} action="#">
                 <div className="form-group">
                     <label>Name:</label>
@@ -31,15 +42,37 @@ export class VisitingCard extends React.Component<RouteComponentProps<{}>, {}> {
 
                 <div className="form-group">
                     <label>Phone:</label>
-                    <input type="number" className="form-control" id="phone" />
+                    <input type="number" className="form-control" id="phone"  />
                 </div>
 
 
                 <div className="checkbox">
                  <label><input type="checkbox" id="willAttend"/> I want to come to party!</label>
                   </div>
-                <button type="submit" className="btn btn-default">Submit</button>
+                <button type="submit" className="btn btn-default" data-toggle="modal" data-target="#exampleModal">Submit</button>
             </form>
+
+            <div className="modal fade" id="exampleModal"  role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Result</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                           Sucsesfully add new client to database!
+      </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+ </div>
+
         );
     }
 
