8707d2f68131e2e7e6bd53960632b221f2c571cc
FWidel <widelfranciszek@gmail.com>
2018-07-16 18:56:56 +0200

    Add searching guest and receiving list of found guest from server

--- TimeSheetList/ClientApp/routes.tsx
+++ TimeSheetList/ClientApp/routes.tsx
@@ -3,8 +3,10 @@ import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { VisitingCard } from './components/VisitingCard';
 import { GuestList } from './components/GuestList';
+import { SearchGuest } from './components/FindGuest';
 
 export const routes = <Layout>
     <Route exact path='/' component={ VisitingCard } />
-    <Route path='/counter' component={ GuestList } />
+    <Route path='/guestList' component={GuestList} />
+    <Route path='/search' component={SearchGuest} />
 </Layout>;
